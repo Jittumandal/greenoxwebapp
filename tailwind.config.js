0825231943794be31b6d7d66e4a22770aa2fc387
Jittumandal <jitendrakumar.designer@gmail.com>
2025-11-24 23:48:27 +0530

    jsiob

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        "./src/**/*.{js,jsx,ts,tsx}",
+        "./index.html",
+        "./src/**/*.{js,jsx,ts,tsx,html}",
+        "./public/**/*.{html,js}",
     ],
     theme: {
         extend: {},
     },
     plugins: [],
 }
-
