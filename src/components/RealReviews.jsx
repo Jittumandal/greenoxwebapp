2fa8bba626ecba07d7dabc31de932cc689f14f99
Jittumandal <jitendrakumar.designer@gmail.com>
2025-11-26 21:54:05 +0530

    testmonila

--- src/components/RealReviews.jsx
+++ src/components/RealReviews.jsx
@@ -2,13 +2,13 @@ import React, { useEffect, useRef } from "react";
 
 const testimonials = [
   {
-    text: "As a fitness enthusiast, I appreciate the protein-packed options at GreeNox. The Soya Paneer Rice Bowl is my favorite - it's both nutritious and delicious. Plus, the cleanliness and hygiene at their food production unit are top-notch.",
+    text: "Greenox has quickly become my top choice for a wholesome and fulfilling meal. Their dedication to sustainability, freshness, and high-quality ingredients truly stands out. It’s more than just great food—it’s a commitment to a healthier, greener lifestyle.",
     name: "Akash",
     role: "Designation Name",
     img: "/img/Akash.jpg",
   },
   {
-    text: "As a fitness enthusiast, I appreciate the protein-packed options at GreeNox. The Soya Paneer Rice Bowl is my favorite - it's both nutritious and delicious. Plus, the cleanliness and hygiene at their food production unit are top-notch.",
+    text: "I’ve been a loyal GreeNox customer for over two years, and their delicious, nutritious dishes keep me coming back. Their plant-based menu has truly been a game-changer for my lifestyle, and every visit feels welcoming thanks to their warm and inviting atmosphere.",
     name: "Aditya Chaudhary",
     role: "Designation Name",
     img: "/img/Aditya.jpg",
@@ -20,7 +20,7 @@ const testimonials = [
     img: "/img/Adeesh.jpg",
   },
   {
-    text: "As a fitness enthusiast, I appreciate the protein-packed options at GreeNox. The Soya Paneer Rice Bowl is my favorite - it's both nutritious and delicious. Plus, the cleanliness and hygiene at their food production unit are top-notch.",
+    text: "As a fitness enthusiast, I truly value the protein-rich meals GreeNox offers. Their Soya Paneer Rice Bowl is my absolute favorite—nutritious, delicious, and perfectly balanced. I’m also consistently impressed by the exceptional hygiene and cleanliness maintained at their food production unit. GreeNox sets a high standard!",
     name: "Kinjalika Verma",
     role: "Designation Name",
     img: "/img/Kinjalika.jpg",
